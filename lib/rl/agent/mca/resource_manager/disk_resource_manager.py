9627ae9fade24cc59848bd75bc41fac34b31149f
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-10-27 06:58:49 +0300

    - Implemented absolute min space

--- lib/rl/agent/mca/resource_manager/disk_resource_manager.py
+++ lib/rl/agent/mca/resource_manager/disk_resource_manager.py
@@ -8,10 +8,11 @@ from .resource_manager import MCResourceManager
 
 class DiskResourceManager(MCResourceManager):
 
-	def __init__(self, min_remaining_space=0.1, path: str = "./"):
+	def __init__(self, min_remaining_space=0.1, min_abs_remaining_space: float = None, path: str = "./"):
 		super().__init__()
 		Logger.info(f"Initializing Disk Resource Manager with min_remaining_space={min_remaining_space}, path={path}")
 		self.__min_remaining_space = min_remaining_space
+		self.__min_abs_remaining_space = min_abs_remaining_space
 		self.__path = path
 
 	def __get_disk_space(self) -> float:
@@ -27,6 +28,10 @@ class DiskResourceManager(MCResourceManager):
 		space = self.__get_disk_space()
 		percentage = space/resource
 		available = percentage > self.__min_remaining_space
+
+		if self.__min_abs_remaining_space is not None:
+			available = space > self.__min_abs_remaining_space
+
 		if not available:
 			Logger.warning(f"Disk space too low. Only {space: .2f} GiB({percentage*100: .2f}% of {resource: .2f} GiB) remaining.")
 		return available
