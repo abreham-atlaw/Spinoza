a58e9fb463d83be50fb98f3cfffcebca90dc67e9
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-10-28 15:02:20 +0300

    - Added it-57-10

--- core/Config.py
+++ core/Config.py
@@ -322,6 +322,7 @@ class RunnerStatsBranches:
 	it_57_6 = "it_57_6"
 	it_57_8 = "it_57_8"
 	it_57_9 = "it_57_9"
+	it_57_10 = "it_57_10"
 
 	it_58_6 = "it_58_6"
 
@@ -381,6 +382,7 @@ class RunnerStatsBranches:
 		it_57_6,
 		it_57_8,
 		it_57_9,
+		it_57_10,
 		it_58_6,
 		it_60_6,
 		it_60_7,
