932943075bc5bb8f630b65fd5fe9fd32c1b199e9
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-12-04 08:45:50 +0300

    - MAPLOSS_FS_MODELS_PATH = "/Apps/RTrader/maploss/it-79/"

--- core/Config.py
+++ core/Config.py
@@ -224,7 +224,7 @@ PREDICTION_MODELS = [
 	ARA_MODEL_CONFIG
 ]
 
-MAPLOSS_FS_MODELS_PATH = "/Apps/RTrader/maploss/it-56/"
+MAPLOSS_FS_MODELS_PATH = "/Apps/RTrader/maploss/it-79/"
 
 
 class ResourceCategories:
