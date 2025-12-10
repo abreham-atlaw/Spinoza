0cf617b06c89b61f23c7262c4378d0183950ce5e
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-12-10 19:55:07 +0300

    - it-86 Configs

--- core/Config.py
+++ core/Config.py
@@ -143,7 +143,7 @@ AGENT_KALMAN_ALPHA = 0.05
 AGENT_KALMAN_BETA = 0.01
 AGENT_MA_WINDOW_SIZE = 64
 AGENT_USE_LASS = True
-AGENT_LASS_MODEL_FS_PATH = "/Apps/RTrader/abrehamalemu-spinoza-lass-training-cnn-31-it-11-tot.0.zip"
+AGENT_LASS_MODEL_FS_PATH = "/Apps/RTrader/abrehamalemu-spinoza-lass-training-cnn-28-it-11-tot.0.zip"
 AGENT_USE_SMOOTHING = not MARKET_STATE_SMOOTHING
 AGENT_CRA_SIZE = 5
 AGENT_CRA_DISCOUNT = 0.7
@@ -224,7 +224,7 @@ PREDICTION_MODELS = [
 	ARA_MODEL_CONFIG
 ]
 
-MAPLOSS_FS_MODELS_PATH = "/Apps/RTrader/maploss/it-85/"
+MAPLOSS_FS_MODELS_PATH = "/Apps/RTrader/maploss/it-86/"
 
 
 class ResourceCategories:
@@ -421,7 +421,7 @@ class RunnerStatsBranches:
 		it_86_6
 	]
 
-	default = it_85_6
+	default = it_86_6
 
 
 class RunnerStatsLossesBranches:
@@ -544,7 +544,7 @@ class RunnerStatsLossesBranches:
 		it_86_0
 	]
 
-	default = it_85_0
+	default = it_86_0
 
 
 
