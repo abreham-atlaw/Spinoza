4ad6b92505bd3f484291eeafcaaa797367af7ff0
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-12-11 06:11:01 +0300

    - Fixed target instrument labeling in stats.py

--- temp/stats.py
+++ temp/stats.py
@@ -291,8 +291,16 @@ def draw_graph_live(root_node, depth=None, top=None, visited=False, state_reposi
 			instant_value = f"\n{node.instant_value: .4f}"
 			label = f"{total_value}{instant_value}"
 			if state_repository is not None:
-				current_price = state_repository.retrieve(node.id).market_state.get_current_price('AUD', 'USD')
-				previous_price = state_repository.retrieve(node.id).market_state.get_state_of("AUD", "USD")[-2]
+				from core.agent.agents.dnn_transition_agent import TraderDNNTransitionAgent
+
+				target_instrument = TraderDNNTransitionAgent._get_target_instrument(
+					state_repository.retrieve(node.parent.parent.id),
+					None,
+					state_repository.retrieve(node.id)
+				)
+
+				current_price = state_repository.retrieve(node.id).market_state.get_current_price(*target_instrument)
+				previous_price = state_repository.retrieve(node.id).market_state.get_state_of(*target_instrument)[-2]
 				label += f"\n{format(current_price - previous_price, '.1e').replace('e-0', 'e-')}"
 				label += f"\n{current_price: .4f}"
 			return label
