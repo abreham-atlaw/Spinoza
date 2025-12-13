e171561fab78ff1b8e5b0f543c702df73474756b
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-12-13 16:50:56 +0300

    - Implemented and Tested sinusoidal_decomposer.py

---
+++ test/core/utils/research/data/prepare/utils/sinusoidal_decomposer_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+import pandas as pd
+
+import os
+
+from core import Config
+from core.utils.research.data.prepare.utils.sinusoidal_decomposer import SinusoidalDecomposer
+
+
+class SinusoidalDecomposerTest(unittest.TestCase):
+
+	def setUp(self):
+		self.decomposer = SinusoidalDecomposer(layer_indifference_threshold=0.05)
+
+	def test_optimize(self):
+		df = pd.read_csv(os.path.join(Config.BASE_DIR, "temp/Data/AUD-USD-10k.csv"))
+		x = df["c"].to_numpy()[::2]
+		y = self.decomposer.decompose(x)
