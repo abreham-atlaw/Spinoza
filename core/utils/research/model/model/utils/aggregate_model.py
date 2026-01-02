06b89ebe840a8cea0c7565b02295cc6ed7c56b8c
Abreham Atlaw <abreham.atlaw@yahoo.com>
2026-01-02 14:28:00 +0300

    - Migrated aggregate_model.py to SpinozaModule - Added optional softmax for processing raw logits

--- core/utils/research/model/model/utils/aggregate_model.py
+++ core/utils/research/model/model/utils/aggregate_model.py
@@ -3,29 +3,46 @@ import typing
 import torch
 import torch.nn as nn
 
+from core.utils.research.model.layers import ReverseSoftmax
 from core.utils.research.model.model.savable import SpinozaModule
 from lib.utils.logger import Logger
 
 
-class AggregateModel(nn.Module):
+class AggregateModel(SpinozaModule):
 
 	def __init__(
 			self,
-			model: nn.Module,
+			model: SpinozaModule,
 			bounds: typing.Union[typing.List[float], torch.Tensor],
 			a: float,
 			y_extra_len: int = 1,
-			temperature: float = 1e-5
+			temperature: float = 1e-5,
+			softmax: bool = False
 	):
-		super().__init__()
+
+		self.args = {
+			"model": model,
+			"bounds": bounds,
+			"a": a,
+			"y_extra_len": y_extra_len,
+			"temperature": temperature,
+			"softmax": softmax
+		}
+		super().__init__(
+			input_size=model.input_size if isinstance(model, SpinozaModule) else None,
+			auto_build=False
+		)
 		self.model = model
 		self.raw_bounds, self.bounds = self.__prepare_bounds(bounds)
 		self.a = a
 		self.n = int(1.0 / a)
 		self.y_extra_len = y_extra_len
 		self.temperature = temperature
-		self.softmax = nn.Softmax(dim=-1)
-		Logger.info(f"Initializing AggregateModel with a={a}")
+		self.temperature_softmax = nn.Softmax(dim=-1)
+
+		self.softmax, self.reverse_softmax = (nn.Softmax(dim=-1), ReverseSoftmax(dim=-1)) if softmax else (nn.Identity(), nn.Identity())
+
+		Logger.info(f"Initializing AggregateModel with a={a}, softmax={softmax}")
 
 	def __prepare_bounds(self, bounds: typing.Union[typing.List[float], torch.Tensor]) -> torch.Tensor:
 		if isinstance(bounds, typing.List):
@@ -64,7 +81,7 @@ class AggregateModel(nn.Module):
 		return x
 
 	def __apply_certainty(self, x: torch.Tensor) -> torch.Tensor:
-		return self.softmax(x/self.temperature)
+		return self.temperature_softmax(x / self.temperature)
 
 	def aggregate(self, x: torch.Tensor) -> torch.Tensor:
 
@@ -95,13 +112,19 @@ class AggregateModel(nn.Module):
 				self.aggregate_y(y[:,i])
 				for i in range(y.shape[1])
 			], dim=1)
+
 		return torch.concatenate(
-			[self.aggregate(y[..., :y.shape[-1] - self.y_extra_len]), y[..., y.shape[-1] - self.y_extra_len:]],
+			[
+				self.reverse_softmax(self.aggregate(self.softmax(y[..., :y.shape[-1] - self.y_extra_len]))),
+				y[..., y.shape[-1] - self.y_extra_len:]
+			],
 			dim=-1
 		)
 
-	def forward(self, x: torch.Tensor) -> torch.Tensor:
-
+	def call(self, x: torch.Tensor) -> torch.Tensor:
 		y = self.model(x)
 
 		return self.aggregate_y(y)
+
+	def export_config(self) -> typing.Dict[str, typing.Any]:
+		return self.args
