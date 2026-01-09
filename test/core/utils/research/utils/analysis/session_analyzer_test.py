3c5a6f960fefcbee581c5de8c61e5bf6fac1f09a
Abreham Atlaw <abreham.atlaw@yahoo.com>
2026-01-09 06:14:35 +0300

    - Implemented and Tested channel plotting in session_analyzer.py

--- test/core/utils/research/utils/analysis/session_analyzer_test.py
+++ test/core/utils/research/utils/analysis/session_analyzer_test.py
@@ -27,8 +27,8 @@ class SessionAnalyzerTest(unittest.TestCase):
 		)
 
 	def test_plot_sequence(self):
-		self.session_analyzer.plot_sequence(checkpoints=[2, 6], instrument=("AUD", "USD"))
-		self.session_analyzer.plot_sequence(checkpoints=[2, 6], instrument=("USD", "ZAR"))
+		self.session_analyzer.plot_sequence(checkpoints=[2, 6], instrument=("AUD", "USD"), channels=("c", 'l', 'h'))
+		# self.session_analyzer.plot_sequence(checkpoints=[2, 6], instrument=("USD", "ZAR"))
 
 	def test_plot_timestep_sequence(self):
 		self.session_analyzer.plot_timestep_sequence(i=0, instrument=("AUD", "USD"))
