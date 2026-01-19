a35a5dff8f33bddeedcce72bf7679f337f472a76
Abreham Atlaw <abreham.atlaw@yahoo.com>
2026-01-19 22:00:25 +0300

    - Added Conditional DirectProbabilityDistributionAgent Usage

--- core/agent/agents/agent.py
+++ core/agent/agents/agent.py
@@ -1,12 +1,15 @@
 from .action_choice_trader.action_choice_trader import ActionChoiceTrader
 from .cra import CumulativeRewardTraderAgent
 from .drmca import TraderDeepReinforcementMonteCarloAgent
+from .direct_probability_distribution_agent import DirectProbabilityDistributionAgent
 from .montecarlo_agent import TraderMonteCarloAgent, ReflexAgent
 from core import Config
 
 
 bases = (
     (ReflexAgent,) if Config.AGENT_MCA_USE_REFLEX else ()
+) + (
+	(DirectProbabilityDistributionAgent,) if Config.AGENT_USE_DIRECT_DISTRIBUTION else ()
 ) + (
     CumulativeRewardTraderAgent,
     TraderDeepReinforcementMonteCarloAgent,
